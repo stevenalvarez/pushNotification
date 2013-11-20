373a80376f2992e731275297ca0c95269e7ff732
jhonny <steven.alvarez.v@gmail.com>
2013-11-20 16:59:54 -0400

    asdf

--- www/js/index.js
+++ www/js/index.js
@@ -56,7 +56,7 @@ var app = {
         }
         
         //notificaion local TEST
-var d = new Date();
+/*var d = new Date();
     d = d.getTime() + 30*1000; //60 seconds from now
     d = new Date(d);
 
@@ -77,7 +77,35 @@ function foreground(id){
 function background(id){
     alert("background");
     console.log("I WAS IN THE BACKGROUND ID="+id)
-}
+}*/
+
+// Schedules a local notification to be triggered after 5 seconds
+window.plugins.localNotification.add({
+    fireDate        : Math.round(new Date().getTime()/1000 + 30),
+    alertBody       : "This is a local notification.",
+    action          : "View",
+    repeatInterval  : "daily",
+    soundName       : "beep.caf",
+    badge           : 0,
+    notificationId  : 123,
+    foreground      : function(notificationId){ 
+        alert("Hello World! This alert was triggered by notification " + notificationId); 
+    },
+    background  : function(notificationId){
+        alert("Hello World! This alert was triggered by notification " + notificationId);
+    }           
+});
+
+// cancel notificationId = 1234
+window.plugins.localNotification.cancel(1234, callback);
+
+// cancel all notifications
+window.plugins.localNotification.cancelAll(callback);
+
+// set badge number to 3
+window.plugins.localNotification.setBadgeNumber(3);
+
+
         //notificaion local TEST
         
     },
