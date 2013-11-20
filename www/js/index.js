10bf12311e1ce2082ab712e243481fd474b2c271
jhonny <steven.alvarez.v@gmail.com>
2013-11-20 16:12:11 -0400

    test sigue

--- www/js/index.js
+++ www/js/index.js
@@ -54,6 +54,25 @@ var app = {
             alert("Register called ios");
             pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
         }
+        
+        //notificaion local TEST
+        alert("Manual01");
+        var d = new Date();
+        d = d.getTime() + 10*1000; //60 seconds from now
+        d = new Date(d);
+        
+        alert("Manual02");
+        window.plugins.localNotification.add({
+        date: d, // your set date object
+        message: 'Hello world!',
+        repeat: 'weekly', // will fire every week on this day
+        badge: 0,
+        foreground:'foreground',
+        background:'background'
+        });
+        alert("Manual03");
+        //notificaion local TEST
+        
     },
     // result contains any message sent from the plugin call
     successHandler: function(result) {
