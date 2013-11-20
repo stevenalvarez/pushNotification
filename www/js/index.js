531c0f505bf24644bb24187c9604706524620df9
jhonny <steven.alvarez.v@gmail.com>
2013-11-20 16:29:12 -0400

    haber asi

--- www/js/index.js
+++ www/js/index.js
@@ -56,19 +56,26 @@ var app = {
         }
         
         //notificaion local TEST
-        alert("Manual01");
-        var d = new Date();
-        d = d.getTime() + 10*1000; //60 seconds from now
-        d = new Date(d);
-        
-        alert("Manual02");
-        window.plugins.localNotification.add({
-        date: d, // your set date object
-        message: 'Hello world!',
-        repeat: 'weekly', // will fire every week on this day
-        badge: 0
-        });
-        alert("Manual03");
+var d = new Date();
+    d = d.getTime() + 5*1000; //60 seconds from now
+    d = new Date(d);
+
+window.plugins.localNotification.add({
+    date: d, // your set date object
+    message: 'Hello world!',
+    repeat: 'weekly', // will fire every week on this day
+    badge: 1,
+    foreground:'foreground',
+    background:'background',
+    sound:'sub.caf'
+});
+
+function foreground(id){
+    console.log("I WAS RUNNING ID="+id);
+}
+function background(id){
+    console.log("I WAS IN THE BACKGROUND ID="+id)
+}
         //notificaion local TEST
         
     },
